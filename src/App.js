64a66fa5ed9840c72faaa88a0220ac2303371874
Pedro Bergamo <pedrobergamo89@gmail.com>
2022-09-13 22:28:39 +0300

    fix fonts and icon

--- src/App.js
+++ src/App.js
@@ -17,35 +17,35 @@ function App() {
       <img width='100%' src={inke2} alt='inke2'/>
       <div className='about'>
       <h1>About</h1>
-        <img width='100%' src={inke1} alt='inke1'/>
+        <img width="100%" src={inke1} alt='inke1'/>
 
-        <p>
-        Download <a className="link" download="inke-launi.png" href={inke1} rel="noreferrer" target="_blank" >photo</a>.</p>
-        <p>
+        <p style={{textAlign: 'center'}}>
+        Download <a className="link" download="inke-launi.png" href={inke1} rel="noreferrer" target="_blank" >photo</a>.
+        </p>
+        <p style={{textAlign: 'center'}}>
         Credits: Samuel Järvinen.
         </p>
         <p>
         Inke Launi on tammikuussa 2022 perustettu helsinkiläinen alternative rock -bändi. Bändin laulaja työsti pitkään kappaleitaan laulaja-lauluntekijä –otteella, mutta kaipasi debyyttisinglensä julkaisun jälkeen rouheampaa soundia ja bändin ympärilleen. Jäsenten valinnassa tärkeäksi tekijäksi nousi yhteinen halu tehdä jotakin erilaista ja riipivää. Vastakohdat täydentävät toisiaan, kun bändin rock-soundi kietoutuu laulajan runollisen tulkinnan ympärille tummia tarinoita kannattelemaan.
         Yhtyeen esikoisalbumin julkaisu on parhaillaan työn alla. Maailmojen Välissä –esikoisalbumilla käsitellään sisäisen maailman ristiriitoja ja kahtiajakautuneisuutta. Albumi nauhoitettiin lähes kokonaan livenä ja soundimaailmassa pyrittiin palaamaan ajassa taaksepäin. Musiikin luonnolliset vivahteet ja säröt saavat kuulua, mikä tekee albumista rohkean ja erottuvan.
         Livenä Inke Launi tarjoaa shown, joka vie yleisön riemun ja tuskan rajamaille. Orgaanisessa estetiikassa jokainen elementti merkitsee ja sama raa’an muhkea soundi on läsnä niin livenä kuin levylläkin. Bändin dynamiikkaa on kuvailtu hyvin erityiseksi ja kappaleita luonnehdittu traagisen jylhiksi. Inke Launin musiikki on spirituaalinen kokemus, joka todella pysäyttää hetken.
-        <p>
         </p>
+        <p style={{textAlign: 'center'}}>
         KOKOONPANO
-        <p>
         </p>
+        <p style={{textAlign: 'center'}}>
         Elina Launimaa – laulu
-        <p>
         </p>
+        <p style={{textAlign: 'center'}}>
         Pedro Bergamo – sähkökitara
-        <p>
         </p>
+        <p style={{textAlign: 'center'}}>
         Nuutti Autio – basso
-        <p>
         </p>
+        <p style={{textAlign: 'center'}}>
         Niilo Suihko – rummut
-        <p>
         </p>
-        </p>
+
         </div>
         <h1>Contact</h1>
         <Social/>
