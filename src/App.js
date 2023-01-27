2c0baf0d7d3e6ccc63d1baa1573de6b45ef8607b
Pedro Bergamo <pedrobergamo89@gmail.com>
2023-01-27 10:16:57 +0200

    Setting the splash screen to screen size

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import './css/App.css';
-import SplashVideo from "./Resources/splash-video.mp4";
 import Footer from "./components/Footer.js"
 import Bio from "./components/bio.js"
-
+import Splash from "./components/splashScreen.js"
 import Gigs from "./components/gigs.js"
-import Logo from "./Resources/logo.svg";
-
-import virta from "./images/virta-logo.png";
-var VirtaVideo = "https://www.youtube.com/watch?v=WcmWrsbxXYA";
 
 function App() {
   return (
       <div className="App">
-        <div className="splash-video">
-
-        <video className="splash-video" autoPlay loop muted>
-          <source src={SplashVideo} type="video/mp4"/>
-        </video>
-
-        <div className="logo">
-          <img src={Logo} width="300px" alt=""/>
-        </div>
-
-          <div className="video-button">
-          <img src={virta} width="50%" alt=""/>
-              <a href={VirtaVideo} target= "_blank" rel="noreferrer">
-              <button type="button">Katso nyt</button>  </a>
-        </div>
-      </div>
+      <Splash/>
       <div className="container">
         <Gigs/>
         <Bio/>
       </div>
-
       <Footer/>
   </div>
   );
---
+++ src/components/splashScreen.js
@@ -0,0 +1,30 @@
+import React from "react"
+import "../css/components.css"
+
+import SplashVideo from "../Resources/splash-video.mp4";
+import Logo from "../Resources/logo.svg";
+
+import virta from "../images/virta-logo.png";
+var VirtaVideo = "https://www.youtube.com/watch?v=WcmWrsbxXYA";
+
+export default class SlashScreen extends React.Component{
+  render(){
+    return(
+        <div className="video-container">
+        <video className="splash-video" autoPlay loop muted>
+          <source src={SplashVideo} type="video/mp4"/>
+        </video>
+
+        <div className="logo">
+          <img src={Logo} width="300px" alt=""/>
+        </div>
+
+          <div className="video-button">
+          <img src={virta} width="50%" alt=""/>
+              <a href={VirtaVideo} target= "_blank" rel="noreferrer">
+              <button type="button">Katso nyt</button>  </a>
+        </div>
+      </div>
+    );
+  }
+}
