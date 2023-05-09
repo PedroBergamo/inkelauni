45e3a3e6cfdbca9095f95f5d09151b51457cbf2b
Pedro Bergamo <pedrobergamo89@gmail.com>
2023-05-09 14:54:45 +0300

    Add new gigs

--- src/components/gigs.js
+++ src/components/gigs.js
@@ -8,7 +8,65 @@ export default class Gigs extends React.Component {
       return(
         <div className="keikat">
         <h1>Keikat</h1>
-        <h3> Meneet Keikat</h3>
+
+        <Row>
+          <Col className="date">
+          <h2>29.6.2023</h2>
+          <div>
+            <p className="text-left">Lepakkomies</p>
+            <p className="text-left">Helsinki</p>
+          </div>
+          </Col>
+
+        </Row>
+        <Row>
+          <Col className="date">
+          <h2>26.7.2023</h2>
+          <div>
+            <p className="text-left">Poppari</p>
+            <p className="text-left">Jyväskylä</p>
+          </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col className="date">
+          <h2>25.7.2023</h2>
+          <div>
+            <p className="text-left">Hönö</p>
+            <p className="text-left">Tampere</p>
+          </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="date">
+          <h2>28.7.2023</h2>
+          <div>
+            <p className="text-left">Hard Rock House</p>
+            <p className="text-left">Helsinki</p>
+          </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="date">
+          <h2>25.8.2023</h2>
+          <div>
+            <p className="text-left">Bar Loose</p>
+            <p className="text-left">Helsinki</p>
+          </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="date">
+          <h2>25.1.2024</h2>
+          <div>
+            <p className="text-left">On The Rocks</p>
+            <p className="text-left">Helsinki</p>
+          </div>
+          </Col>
+        </Row>
+
+        <h3> Menneet Keikat</h3>
         <Row>
         <Col xs={12} lg={6} className="d-none d-lg-block">
         <img src={Elina} width="100%" alt=""/>
@@ -17,10 +75,10 @@ export default class Gigs extends React.Component {
                 <Row>
 
                   <Col className="date">
-                    <h2>24.2</h2>
+                    <h2>24.2.2023</h2>
                     <div>
-                      <p>Pub Virasto</p>
-                      <p>Lahti</p>
+                      <p className="text-left">Pub Virasto</p>
+                      <p className="text-left">Lahti</p>
                     </div>
                     </Col>
                 {/*
@@ -36,10 +94,10 @@ export default class Gigs extends React.Component {
 
                 <Row>
                   <Col className="date">
-                  <h2>25.2</h2>
+                  <h2>25.2.2023</h2>
                   <div>
-                    <p>Oranssi</p>
-                    <p>Helsinki</p>
+                    <p className="text-left">Oranssi</p>
+                    <p className="text-left">Helsinki</p>
                   </div>
                   </Col>
 
@@ -47,20 +105,20 @@ export default class Gigs extends React.Component {
 
                 <Row>
                   <Col className="date">
-                  <h2>01.3</h2>
+                  <h2>01.3.2023</h2>
                   <div>
-                    <p>Lepakkomies</p>
-                    <p>Helsinki</p>
+                    <p className="text-left">Lepakkomies</p>
+                    <p className="text-left">Helsinki</p>
                   </div>
                   </Col>
                 </Row>
 
                 <Row>
                   <Col className="date">
-                  <h2>03.3</h2>
+                  <h2>03.3.2023</h2>
                   <div>
-                    <p>Kaivuri</p>
-                    <p>Kuopio</p>
+                    <p className="text-left">Kaivuri</p>
+                    <p className="text-left">Kuopio</p>
                   </div>
                   </Col>
                   <Col className="gig-button">
@@ -70,20 +128,20 @@ export default class Gigs extends React.Component {
 
                 <Row>
                   <Col className="date">
-                  <h2>04.3</h2>
+                  <h2>04.3.2023</h2>
                   <div>
-                    <p>Tuba</p>
-                    <p>Oulu</p>
+                    <p className="text-left">Tuba</p>
+                    <p className="text-left">Oulu</p>
                   </div>
                   </Col>
                 </Row>
 
                 <Row>
                   <Col className="date">
-                    <h2>09.3</h2>
+                    <h2>09.3.2023</h2>
                     <div>
-                      <p>On the Rocks</p>
-                      <p>Helsinki</p>
+                      <p className="text-left">On the Rocks</p>
+                      <p className="text-left">Helsinki</p>
                     </div>
                     </Col>
                 </Row>
