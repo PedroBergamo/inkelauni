130488c3d199aac72a26a809c6faff9d02c48a87
Pedro Bergamo <pedrobergamo89@gmail.com>
2024-02-13 12:58:12 +0200

    Set flower

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './css/App.css';
-import Footer from "./components/Footer.js"
-import Bio from "./components/bio.js"
 import Splash from "./components/splashScreen.js"
 
 function App() {
   return (
       <div className="App">
       <Splash/>
-      <div className="container">
-        <Bio/>
-      </div>
-      <Footer/>
   </div>
   );
 }
--- src/components/splashScreen.js
+++ src/components/splashScreen.js
@@ -1,28 +1,14 @@
 import React from "react"
 import "../css/components.css"
 
-import SplashVideo from "../resources/splash-video-2.mp4";
-import Logo from "../resources/logo.svg";
-
-import virta from "../images/virta-logo.png";
-var VirtaVideo = "https://www.youtube.com/watch?v=WcmWrsbxXYA";
+import Flower from "../resources/I-Flower.svg";
 
 export default class SlashScreen extends React.Component{
   render(){
     return(
-        <div className="video-container">
-        <video className="splash-video" autoPlay loop muted>
-          <source src={SplashVideo} type="video/mp4"/>
-        </video>
-
-        <div className="logo">
-          <img src={Logo} width="300px" alt=""/>
-        </div>
-
-          <div className="video-button">
-          <img src={virta} width="50%" alt=""/>
-              <a href={VirtaVideo} target= "_blank" rel="noreferrer">
-              <button type="button">Katso</button>  </a>
+        <div className="splash-container">
+        <div>
+          <img src={Flower} width="300px" alt=""/>
         </div>
       </div>
     );
